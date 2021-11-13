a6d8a6f03ab3fec7676479e3372afafbc3d1d4b5
ElenaLj <elenaljubijankic@gmail.com>
2021-11-13 16:27:08 +0100

    insert frecce

--- js/script.js
+++ js/script.js
@@ -11,14 +11,6 @@ const imagesArray = [
     "img/05.jpg", // indice 4
 ];
 
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-]
-
 // 2) inserisco array in thumbnail
 
     // 2.1) indico a JS dove andare ad inserire array creando una variabile per il mio div di punta
@@ -32,39 +24,60 @@ const myThumbnail = document.querySelector(".thumbnail");
     // 2.2b) creo una variabile let vuota che andrò ad inserire nel ciclo
 // let thumbnailContent = "";
 
-// for (let i = 0; i < imagesArray.length; i++) {
-//     thumbnailContent += `
-//     <div class="img-container">
-//         <img src="${imagesArray[i]}" alt="">
-//     </div>
-//     `
-// }
-// console.log(thumbnailContent);
+ for (let i = 0; i < imagesArray.length; i++) {
+    myThumbnail.innerHTML += `
+     <div class="img-container">
+         <img src="${imagesArray[i]}" alt="">
+     </div>
+     `
+ }
+//  console.log(thumbnailContent);
 
     // 2.3) ora vado ad inserire nel div di punta il contenuto dinamico creato col ciclo for
 // myThumbnail.innerHTML = thumbnailContent;
 
-// **************/ PARTE CHE NON FUNZIONA PERCHE' NASCONDE LE MIE FRECCE
 
-
-// ***************** INSERENDO DIRETTAMENTE NEL CICLO SI VEDONO
-for (let i = 0; i < imagesArray.length; i++) {
-    myThumbnail.innerHTML += `
-    <div class="img-container">
-        <img src="${imagesArray[i]}" alt="">
-    </div>
-    `
-}
 
     // 2.4) devo aggiungere anche la classe active 
-const imgActive = document.querySelector(".img-container");
-imgActive.className = "img-container active";
+// const imgActive = document.querySelector(".img-container");
+// imgActive.className = "img-container active";
 
+//selezioniamo tutto per rendere il primo attivo
+const imgContainer = document.getElementsByClassName("img-container");
+console.log(imgContainer);
+
+// creo una variabile per incrememntare l'indice di volta in volta
+let imgActive = 0; 
+imgContainer[imgActive].classList.add("active");
     
-
 //Quando clicco sulla freccia cambia immagine
-// let up = document.querySelector(".arrow-up");
-// let down = document.querySelector(".arrow-up");
-// myArrow.addEventListener("click", function() {
-//     for (var i = 0 ; )
-// })
+const up = document.querySelector(".arrow-up");
+const down = document.querySelector(".arrow-down");
+
+down.addEventListener("click", function() {
+   if (imgActive < imagesArray.length - 1) {
+        //rimuovere classe active al primo elemento
+        imgContainer[imgActive].classList.remove("active");
+
+        //incrememnto
+        imgActive++;
+
+        //aggiugno classe all'elemento incremmentato
+        imgContainer[imgActive].classList.add("active");
+   }
+
+});
+
+up.addEventListener("click", function (){
+
+    if (imgActive > 0) {
+        //togliere la classe active
+    imgContainer[imgActive].classList.remove("active");
+
+    //incremento l'indice 
+    imgActive--;
+
+    //aggiungo la classe active all'elemento incrementato
+    imgContainer[imgActive].classList.add("active");
+    }
+});
